3942a779dd884185bd7d0e173864a97176e3911e
Daksh <dpatel132@hawk.iit.edu>
2022-02-13 14:25:54 -0600

    Initial Commit

---
+++ Shared/Fractal_Koch_SnowflakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fractal_Koch_SnowflakeApp.swift
+//  Shared
+//
+//  Created by Daksh Patel on 2/13/22.
+//
+
+import SwiftUI
+
+@main
+struct Fractal_Koch_SnowflakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
